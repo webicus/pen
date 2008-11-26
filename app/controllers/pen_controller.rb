ba69084bfd61e5fdace65ecbf53c2a263c14d410
ehw <eric@webicus.com>
2008-11-26 17:41:27 -0500

    v1.2. Issue with chrome and overlays (double click to initiate).

--- app/controllers/pen_controller.rb
+++ app/controllers/pen_controller.rb
@@ -1,29 +1,32 @@
 class PenController < ApplicationController
   layout 'catalog'
-  before_filter :check_authorization, :except => [ :logo,:stacy, :unstacy]
+  # before_filter :check_authorization, :except => [ :logo,:stacy, :unstacy]
   
   LARGE_DIR="/images/pens/large"
   SMALL_DIR="/images/pens/small"
   LARGE_COLORS_DIR="/images/colors/large"
   SMALL_COLORS_DIR="/images/colors/small"
-  
+  TITLE = "Penz by Design - "
   
   def index
+    @title = "Penz by Design - Handcrafted writing instruments for you"
   end
   
   def collector
     @margin_top ="-187px"
-
+    @title = TITLE + "Collector Series"
     @collection = Collection.find_by_title("collector series")
   end
   
   def corporate
+    @title = TITLE + "Corporate Series"
     @margin_top ="-187px"
     @collection = Collection.find_by_title("corporate series")
     
   end
   
   def elite
+    @title = TITLE + "Elite Series"
     @last = nil
     @cnt = 0
     
@@ -32,8 +35,22 @@ class PenController < ApplicationController
   end
   
   def color
+    @title = TITLE + "Colors"
     @margin_top ="-187px"
+    @list_style = "float:left; width:100%;height: 300px; overflow-y: auto;"
     @colors = Color.find(:all, :order => :sequence)
+  end
+  def contact_us
+    @title = TITLE + "Contact Us"
+  end
+  def about_us
+    @title = TITLE + "About Us"
+  end
+  def our_products
+    @title = TITLE + "Our Products"
+  end
+    def accessories
+    @title = TITLE + "Engraving and Accessories"
   end
   
   #used to superimpose and replace images uses
@@ -47,22 +64,25 @@ class PenController < ApplicationController
   # Action called by AJAX link to remote. (called in fade_replace)
   def fade_replace_aj
     item = nil
-
+    
     @style=""
     if params[:color] == 'true'
       dir = LARGE_COLORS_DIR
       item = Color.find_by_id(params[:id])
       @pen_name = item.title
-      @style = "style='margin-right: 45px; margin-left: 45px;'"
+      # 
+      size = 'width:357px;height:101px;padding: 58px 216px;'
     else
+
       dir = LARGE_DIR
       item = Pen.find_by_id(params[:id])   
       @pen_name = Style.find_by_id(item.style_id).title # displayed under the image
+      size = 'width:793px;height:216px;'
     end
     image = item.image_filename
     @cnt = session[:fade_replace_cnt] || 0 #used for odd/even count.
     @image_name = dir + "/" + image + "_lrg.jpg" # used to lookup the image's filename
-    
+    @style = "background-image: url(#{@image_name});background-position:center center;background-repeat:no-repeat;margins:auto; #{size}"
     if @cnt.to_i % 2 == 0
       @from = "_even"
       @to = "_odd"
@@ -70,8 +90,8 @@ class PenController < ApplicationController
       @from = "_odd"
       @to = "_even"
     end
-
-      @cnt = @cnt + 1
+    
+    @cnt = @cnt + 1
     session[:fade_replace_cnt] = @cnt
     @last = params[:id]
   end

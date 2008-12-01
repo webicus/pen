130409da67576fbbbadc11e70b52dce65f2bdd74
ehw <eric@webicus.com>
2008-12-01 16:49:41 -0500

    v1.3.3.1 - routes changed. Live.

--- config/routes.rb
+++ config/routes.rb
@@ -33,8 +33,8 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-#map.connect '',  :controller => 'pen'
-#map.connect '/',  :controller => 'pen'
+map.connect '',  :controller => 'pen'
+map.connect '/',  :controller => 'pen'
 
   # See how all your routes lay out with "rake routes"
 
